76557aa0926bba8d90973614a1876844face4a6a
vaibhav garg <vaibhavvipingarg@gmail.com>
2015-04-24 00:00:01 -0400

    change port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.port || 8081;
+var port = process.env.port || 8080;
 var io = require('socket.io').listen(app.listen(port));
 require('./config.js')(app, io);
 require('./routes.js')(app, io);
